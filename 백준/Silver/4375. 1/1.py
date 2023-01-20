da2c16f30a0762d0ba4e68d18572873cb3651e57
developer_jennifer <heekyoung2000@naver.com>
2023-01-20 11:53:02 +0900

    [Silver III] Title: 1, Time: 860 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        N=int(input())
+    except EOFError:
+        break
+    
+    a,i=0,0
+    while True:
+        a+=10**i
+        if a%N==0:
+            print(i+1)
+            break
+        i+=1
