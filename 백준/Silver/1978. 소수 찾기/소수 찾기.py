3cc665adb75ae5d51c109b0687b3bc32db6dd72c
developer_jennifer <heekyoung2000@naver.com>
2023-01-25 17:25:46 +0900

    [Silver V] Title: 소수 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,10 @@
+N=int(input())
+data=list(map(int,input().split()))
+cnt=0
+for i in data:
+    for j in range(2,i+1):
+        if i%j==0:
+            if j==i:
+                cnt+=1
+            break
+print(cnt)
