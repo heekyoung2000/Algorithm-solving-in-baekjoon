8a43c694ab6c1580d507fb38265483227e4e19fa
developer_jennifer <heekyoung2000@naver.com>
2023-04-10 16:36:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 115260 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,18 @@
+def insertion_sort(list1):
+    for i in range(1,n):
+        j=i
+        tmp=list1[i]
+        while j>0 and list1[j-1]>tmp:
+            list1[j]=list1[j-1] # 큰값을 뒤에 저장
+            j-=1
+        list1[j]=tmp #작은 값을 앞에 저장
+
 n=int(input())
 list1=[]
 for i in range(n):
     number=int(input())
     list1.append(number)
 
-list1.sort()
+insertion_sort(list1)
 for j in list1:
     print(j)
