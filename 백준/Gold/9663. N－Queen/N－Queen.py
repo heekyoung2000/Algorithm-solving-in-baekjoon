5061f15fc6113c977c32df0f47fa48ccdac12056
developer_jennifer <heekyoung2000@naver.com>
2023-04-11 12:06:56 +0900

    [Gold IV] Title: N-Queen, Time: 24924 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,28 @@
+import sys
+n=int(sys.stdin.readline())
+cnt=0
+
+pos= [0]*n #각 열에 배치한 퀸의 위치
+flag_a = [False]*n #각 행에 퀸을 배치했는지 체크
+flag_b = [False]*(n*2-1) #대각선 방향으로 퀸을 배치했는지 체크
+flag_c = [False]*(n*2-1)  #대각선 방향으로 퀸을 배치했는지 체크
+list1=[]
+
+def set(i):
+    for j in range(n):
+        cnt=0
+        if not flag_a[j] and not flag_b[i+j] and not flag_c[i-j+(n-1)]:
+            pos[i]=j
+            if i==n-1:
+                #put()
+                cnt+=1
+                list1.append(cnt)
+            else:
+                flag_a[j]= flag_b[i+j] = flag_c[i-j+(n-1)] = True
+                set(i+1)
+                flag_a[j] = flag_b[i+j] = flag_c[i-j+(n-1)]=False
+
+
+set(0)
+print(sum(list1))
+            
