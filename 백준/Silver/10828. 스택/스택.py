ea43d224e9a76fcd091f7e2148bacffa89563d78
developer_jennifer <heekyoung2000@naver.com>
2023-04-14 11:31:35 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 34144 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+new_list=[]
+def stack(a):
+    if a[0]=="push":
+        new_list.append(a[1])
+    elif a[0]=="top":
+        if len(new_list)==0:
+            print(-1)
+        else:
+            print(new_list[-1])
+    elif a[0]=="size":
+        print(len(new_list))
+    elif a[0]=="empty":
+        if len(new_list)==0:
+            print(1)
+        else:
+            print(0)
+    elif a[0]=="pop":
+        if len(new_list)==0:
+            print(-1)
+        else:
+            print(new_list.pop())       
+        
+
+#s=open('input.txt','rt')
+n = int(sys.stdin.readline())
+
+list1=[]
+for i in range(n):
+    no = list(map(str,sys.stdin.readline().split()))
+    stack(no)
