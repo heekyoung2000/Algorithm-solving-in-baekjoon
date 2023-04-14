3c49a1d531953984d0360b001e69873ecbafd572
developer_jennifer <heekyoung2000@naver.com>
2023-04-14 11:48:31 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+dq = deque() #덱 생성
+
+def deq(n):
+    if n[0]=="push":
+        dq.append(n[1])
+    elif n[0]=="front":
+        if len(dq)==0:
+            print(-1)
+        else:
+            print(dq[0]) #가장 앞에 있는 정수 출력
+    elif n[0]=="back":
+        if len(dq)==0:
+            print(-1)
+        else:
+            print(dq[-1]) #가장 뒤에 있는 정수 출력
+    elif n[0]=="size":
+        print(len(dq))
+    elif n[0]=="empty":
+        if len(dq)==0:
+            print(1)
+        else:
+            print(0)
+    elif n[0]=="pop":
+        if len(dq)==0:
+            print(-1)
+        else:
+            print(dq.popleft())
+
+#s=open('input.txt','rt')
+n = int(sys.stdin.readline())
+for i in range(n):
+    no=list(map(str,sys.stdin.readline().split()))
+    deq(no)
