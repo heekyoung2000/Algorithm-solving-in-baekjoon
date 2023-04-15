356f6b7241f15d368dcab34c18d4d1a8bf1e1ee0
developer_jennifer <heekyoung2000@naver.com>
2023-04-15 16:31:48 +0900

    Update 탑.py

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -21,3 +21,23 @@ def stack(list1):
 list1=list(map(int,sys.stdin.readline().split()))
 for i in stack(list1):
     print(i,end=" ")
+    
+#다른 해설(정답)
+result=[]
+answer=[0]*n # 통신 할 수 없는 경우에 0을 출력해야 하므로 선언할 때 부터 0으로 구성된 배열로 선언
+def stack(list1):
+    for i in range(len(list1)):
+        print(result, answer)
+        while result:
+            if list1[result[-1]] < list1[i]: #현재 값보다 뒤에 있는 값이 더 크면 통신할 수 없으므로 pop실행
+                result.pop()
+            else: #그 외에는 통신이 가능하므로 현재 인덱스에 1을 더한 값을 answer에 저장한다.
+                answer[i]=(result[-1]+1)
+                break
+        result.append(i) #result에 인덱스와 list의 값을 모두 저장한다.   
+
+list1=list(map(int,s.readline().split()))
+stack(list1)
+#print(* answer) #포인터를 사용한 것으로 해당하는 인덱스 주소값을 출력
+for i in answer:
+    print(i,end=" ")
