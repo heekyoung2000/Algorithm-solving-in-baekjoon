40b069f36a12d729c3962feed2684defbf2d81c9
developer_jennifer <heekyoung2000@naver.com>
2023-04-17 20:24:04 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 51444 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,5 +1,5 @@
 # 이진탐색은 정렬이 필수
-# set 함수를 통해 중복 제거 - 내장 함수를 이용한 문제풀이
+# 각 배열을 
 import sys
 
 def exist(lista,listm):
@@ -19,24 +19,3 @@ lista= set(map(int,sys.stdin.readline().split()))
 m=int(sys.stdin.readline())
 listm = list(map(int,sys.stdin.readline().split()))
 exist(lista,listm)
-
-# 이진탐색을 이용한 수찾기 - 재귀를 이용한 이진탐색
-def binary_search(lista,target,start,end):
-    if start > end:
-        return 0
-    mid = (start+end)//2
-    if target==lista[mid]: #target을 lista[mid]와 비교 lista[mid]==target은 lista[mid]를 target에 비교하는 것이므로 오류
-        return 1
-    elif target > lista[mid]:
-        return binary_search(lista,target,mid+1,end)
-    else:
-        return binary_search(lista,target,start,mid-1)
-
-
-s=open("input.txt","rt")
-n=int(s.readline())
-lista= sorted(list(map(int,s.readline().split())))
-m=int(s.readline())
-listm = list(map(int,s.readline().split()))
-for i in listm:
-    print(binary_search(lista,i,0,n-1))
