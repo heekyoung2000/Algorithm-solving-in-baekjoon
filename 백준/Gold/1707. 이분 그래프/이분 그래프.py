e25a928845c3d9d42a6c57c5954487ceef3c6358
developer_jennifer <heekyoung2000@naver.com>
2023-04-26 15:00:50 +0900

    [Gold IV] Title: 이분 그래프, Time: 1296 ms, Memory: 53544 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -2,45 +2,42 @@ from sys import stdin as s
 from collections import deque
 import sys
 sys.setrecursionlimit(10**6)
-
 #s=open("input.txt","rt")
 
-n=int(s.readline())
-
-def dfs(start,visited,graph,group):
-    visited[start] =group #현재 노드의 그룹 저장
-    
-    #인접 노드 탐색
-    for v in graph[start]:
-        if visited[v]==0:
-            result = dfs(v,visited,graph,-group) #-group : 현재 노드의 그룹과 다른 값 전달
+def dfs(start,color):
+    visited[start]=color #현재 노드에 1값 저장 
+    for i in graph[start]:
+        if visited[i]==0: #노드에 방문 하지 않았을 경우
+            result = dfs(i,-color) #인접 노드를 탐색 하면서 -1값을 저장
+            
             if not result:
-                return False        
-        else:
-            if visited[v] == group:
                 return False
+        else:
+            if visited[i]==color:
+                return False
+                 
     return True
 
 
-for _ in range(n):
-    v,e = map(int,s.readline().split())
-    graph = [[] for _ in range(v+1)]
-    visited = [0] * (v+1)
+k=int(s.readline()) #k:테스트 케이스 개수
+
+for i in range(k):#테이스케이스 수 만큼 v,e를 새로 지정해줘야 하므로 for문 안에 있어야 함
+    v,e = map(int,s.readline().split()) #v:정점의 개수, e: 간선의 개수
+    graph=[[] for i in range(v+1)]
+    visited = [0]*(v+1)
     for _ in range(e):
-        a,b = map(int,s.readline().split())
-        graph[a].append(b)
-        graph[b].append(a)
-        
-    for i in range(1, v+1):
-        if visited[i] ==0:
-            result = (dfs(i,visited,graph,1))
-            if not result:
+        u,v=map(int,s.readline().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    
+
+    for i in range(1,v+1): # 모든 간선에 대해 dfs 재귀를 돌려줘야 함
+        if visited[i]==0: #만약 방문하지 않은 노드가 있다면 
+            result = (dfs(i,1)) #dfs를 돌려줌
+            if not result: #result가 false이면 멈춤
                 break
-        
+            
     if result:
         print("YES")
     else:
         print("NO")
-    
-
-    
