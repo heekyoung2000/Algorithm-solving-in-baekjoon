59e62dab3cd4255cc62a8df1e814924f1085befe
developer_jennifer <heekyoung2000@naver.com>
2023-05-10 17:34:37 +0900

    [Silver IV] Title: 상근이의 여행, Time: 244 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,30 @@
+from sys import stdin as s
+from collections import deque
+
+#s=open("input.txt","rt")
+
+T=int(s.readline())
+
+def bfs(tree, start):
+    count=0
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v= queue.popleft()
+        for i in tree[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
+                count+=1
+    print(count)
+
+for i in range(T):
+    
+    n,m = map(int,s.readline().split())
+    flight_list=[[] for _ in range(m+2)]
+    visited = [False for _ in range(m+2)]
+    for i in range(m):
+        go,arrival= map(int,s.readline().split())
+        flight_list[go].append(arrival)
+        flight_list[arrival].append(go)
+    bfs(flight_list,1)
