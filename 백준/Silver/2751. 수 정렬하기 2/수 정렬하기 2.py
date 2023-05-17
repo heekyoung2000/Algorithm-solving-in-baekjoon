410276faaa5732dd712e41679f8a03f238a99e88
developer_jennifer <heekyoung2000@naver.com>
2023-05-17 10:04:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1484 ms, Memory: 77108 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,9 +1,15 @@
-n=int(input())
-list1=[]
-for i in range(n):
-    number=int(input())
-    list1.append(number)
+from sys import stdin as s
+#이분 탐색으로 진행
 
-list1.sort()
-for j in list1:
+
+#s=open("input.txt","rt")
+n=int(s.readline())
+num_list=[]
+# def binary():
+    
+for _ in range(n):
+    num=int(s.readline())
+    num_list.append(num)
+num_list.sort()
+for j in num_list:
     print(j)
