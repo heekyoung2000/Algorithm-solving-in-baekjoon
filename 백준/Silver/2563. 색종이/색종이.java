f91afdee142d7bc4f4ec1d668dfe30840f05cd38
developer_jennifer <heekyoung2000@naver.com>
2023-06-21 13:39:54 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int total = 0;
+        int n = Integer.parseInt(br.readLine()); // 색종이 개수
+        boolean[][] arr= new boolean[101][101]; // 도화지
+
+        for(int i =0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for(int j = a; j<a+10; j++){
+                for(int k = b; k< b+10; k++){
+                    if(!arr[j][k]){
+                        arr[j][k]=true;
+                        total++;
+                    }
+                }
+            }
+        }
+        System.out.println(total);
+
+
+    }
+}
