f2e16a205e88566cfdc0433321cfa30e6ea5055e
developer_jennifer <heekyoung2000@naver.com>
2023-09-27 23:50:09 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,25 @@
+def solution(brown, yellow):
+    answer = []
+    yellow_x =0
+    yellow_y=0
+    
+    for i in range(1, yellow+1):
+        if yellow% i ==0:
+            yellow_x =int(yellow/i)
+            yellow_y = i
+            if yellow_x *2 + yellow_y*2 + 4 == brown:
+                answer.append(yellow_x+2)
+                answer.append(yellow_y+2)
+                
+                return sorted(answer,reverse=True)
+    return answer
+    # total = brown+yellow
+    # result=[]
+    # for i in range(1,total):
+    #     if total%i==0:
+    #         result.append(i)
+    # print(result)
+    # l=result[len(result)//2]
+    # r = total//l
+    # answer = [r,l]
+    # return answer
