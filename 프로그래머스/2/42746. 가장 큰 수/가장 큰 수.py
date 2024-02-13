9ea645a98ecd7dbca86fb30868dcdd1e5afa041f
developer_jennifer <heekyoung2000@naver.com>
2024-02-14 00:47:26 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,4 +1,4 @@
-def solution(numbers): 
-    numbers=list(str(n) for n in numbers)
-    numbers.sort(key=lambda x:x*3,reverse=True)
-    return str(int("".join(numbers)))
+def solution(numbers):
+    new_arr=list(str(n) for n in numbers)
+    new_arr.sort(key=lambda x:x*3,reverse=True)
+    return str(int("".join(new_arr)))
