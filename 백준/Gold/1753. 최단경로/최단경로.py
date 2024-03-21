a31ed6973cd64891e6ac08c9c5dc18338137f1af
developer_jennifer <heekyoung2000@naver.com>
2024-03-21 13:57:08 +0900

    [Gold IV] Title: 최단경로, Time: 644 ms, Memory: 68380 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,37 +1,41 @@
 import sys
-import collections, heapq
-V,E = map(int,sys.stdin.readline().split())
-K = int(sys.stdin.readline().strip())
+import heapq
+
+input = sys.stdin.readline
 INF = int(1e9)
 
-#각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
-graph=[[] for _ in range(V+1)]
-#최단 거리 테이블을 모두 무한으로 초기화
-dis = [INF]*(V+1)
+V,E = map(int,input().split())
+start = int(input().strip())
+
+graph = [[] for i in range(V+1)]
+distance = [INF]* (V+1)
+
 
 for i in range(E):
-    u,v,w=map(int,sys.stdin.readline().split())
+    u,v,w = map(int,input().split())
     graph[u].append((v,w))
-
-def dijkstra(K):
+    
+    
+def dijkstra(start):
     q=[]
-    heapq.heappush(q,(0,K))
-    dis[K]=0
+    heapq.heappush(q,(0,start))
+    distance[start]=0
+    
     while q:
-        dist,now = heapq.heappop(q)
-        if dis[now]<dist:
+        dis,now = heapq.heappop(q)
+        
+        if dis> distance[now]:
             continue
-        #현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost<dis[i[0]]:
-                dis[i[0]]=cost
-                heapq.heappush(q,(cost,i[0]))
+        for g in graph[now]:
+            cost = dis + g[1]
+            if cost<distance[g[0]]:
+                distance[g[0]]=cost
+                heapq.heappush(q,(cost,g[0]))
 
-dijkstra(K)
-
-for i in range(1,V+1):
-    if dis[i]==INF:
+    
+dijkstra(start)
+for j in range(1,V+1):
+    if distance[j]==INF:
         print("INF")
     else:
-        print(dis[i])
+        print(distance[j])
