4aa37e9631d6dfeaec1bf8ff587abf1a0ebf06aa
developer_jennifer <heekyoung2000@naver.com>
2024-04-22 18:09:31 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,19 +1,10 @@
-import sys
-from collections import deque
+N = int(input())
 
-input =sys.stdin.readline
-
-n=int(input())
-num_list=deque()
-num_list.append(0)
-num_list.append(1)
-i=1
-if n==0:
-    print(0)
-else:
-    while n!=i:
-        fibo = num_list[0]+num_list[1]
-        num_list.append(fibo)
-        num_list.popleft()
-        i+=1
-    print(num_list[-1])
+def fibo(N):
+    if N==0:
+        return 0
+    elif N==1:
+        return 1
+    else:
+        return fibo(N-1)+fibo(N-2)
+print(fibo(N))
