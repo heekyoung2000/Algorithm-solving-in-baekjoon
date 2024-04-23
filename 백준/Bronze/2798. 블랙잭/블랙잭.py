6864af65353b022f1306f2bfae264bf1b15bbb76
developer_jennifer <heekyoung2000@naver.com>
2024-04-23 09:30:26 +0900

    [Bronze II] Title: 블랙잭, Time: 136 ms, Memory: 42768 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,20 +1,16 @@
-from sys import stdin as s
-from itertools import combinations,permutations
+import sys
+from itertools import combinations
 
-n,m = map(int,s.readline().split())
-list1 = list(map(int,s.readline().split()))
-combi = list(combinations(list1,3))
-list2=[]
-result=0
-for i in sorted(combi):
-    s = sum(i)
-    if s==m:
-        result=s
-    elif s<m:
-        list2.append(s)
-        
-if result==0:
-    print(max(list2))
-else:
-    print(result)
-    
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+card = list(map(int,input().split()))
+
+result = 0
+for comcard in list(combinations(card,3)):
+    if sum(comcard)>M:
+        continue
+    else:
+        result = max(result,sum(comcard))
+
+print(result)
