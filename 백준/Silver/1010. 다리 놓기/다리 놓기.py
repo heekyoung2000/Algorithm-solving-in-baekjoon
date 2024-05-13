1e1db2f7ef6445fa0c7989bc07e241320279b45b
developer_jennifer <heekyoung2000@naver.com>
2024-05-13 20:46:03 +0900

    [Silver V] Title: 다리 놓기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,20 @@
+import sys
+input =sys.stdin.readline
+
+T= int(input())
+def dfs(n,m):
+    dp=[[1]*(m+1) for _ in range(m+1)]
+    for i in range(2,m+1):
+        dp[1][i]=i
+    for i in range(2,n+1):
+        for j in range(i+1,m+1):
+            dp[i][j]=dp[i-1][j-1]+dp[i][j-1]
+    return dp[n][m]
+        
+
+for _ in range(T):
+    n,m = map(int,input().split())
+    print(dfs(n,m))
+    
+    
+   
