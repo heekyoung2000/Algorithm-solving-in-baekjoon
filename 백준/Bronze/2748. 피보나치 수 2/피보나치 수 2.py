42e8ec5e48a7de53b7e6641a4f0b1b34c508d598
developer_jennifer <heekyoung2000@naver.com>
2024-05-13 18:29:40 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,12 +1,21 @@
-#바텀 업 방식
-from sys import stdin as s
-from collections import deque
-#s=open("input.txt","rt")
-n=int(s.readline())
-d=[0]*100
-d[1]=1
-d[2]=1
-for i in range(3,n+1):
-    d[i]= d[i-1]+d[i-2]
+import sys
+input =sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+n = int(input())
+s = [0]*100
+s[1]=1
+s[2]=1
+def dfs(n):
+    if n==0:
+        return s[n]
     
-print(d[n])
+    elif n==1:
+        return s[n]
+    
+    else:
+        for a in range(3,n+1):
+            s[a] = s[a-1]+s[a-2]
+        return s[n]
+    
+print(dfs(n))
