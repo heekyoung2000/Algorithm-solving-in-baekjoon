cce9997b78c417e4802324d501095e99032cc9a8
developer_jennifer <heekyoung2000@naver.com>
2024-05-17 22:03:59 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,24 +1,30 @@
-#2개를 제외하고 나머지를 더했을때 100이 되는 숫자 찾기
-#9개에서 2개를 뽑아야 함
 import sys
-import random
+input =sys.stdin.readline
 
-def choose(list1):
-    while True:
-        out = random.sample(list1,7)
-        if sum(out)==100:
-            break
-    return out
+def dfs(height):
+    if sum(height)==100:
+        print(*height)
+        return
+    # elif sum(height)>100:
         
-    
-list1=[]
-for i in range(9):
-    list1.append(int(sys.stdin.readline()))
-    
-#2개를 뽑기
-result=choose(list1)
-result.sort()
-for i in result:
-    print(i)    
-    
-    
+
+height = []
+dp = []
+for _ in range(9):
+    n = int(input())
+    height.append(n)
+
+height.sort()
+def dfs(depth,start):
+    if depth==7:
+        if sum(dp)==100:
+            print(* dp,sep="\n")
+            exit()
+        else:
+            return
+    for i in range(start,len(height)):
+        dp.append(height[i])
+        dfs(depth+1,i+1)
+        dp.pop()
+
+dfs(0,0)
