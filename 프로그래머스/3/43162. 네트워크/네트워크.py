cfc9c7c7b118244fcc83deb0fcfe5033401378aa
developer_jennifer <heekyoung2000@naver.com>
2024-06-03 21:41:52 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,26 +1,26 @@
 from collections import deque
 def solution(n, computers):
-    answer=0
-    def bfs(v,visited):
-        queue=deque([v])
-        while queue:
-            j=queue.popleft()
-            for i in range(n):
-                if computers[i][j]==1 and visited[i+1]==False:
-                    queue.append(i)
-                    visited[i+1]=True
-        return
-
-
-    visited=[False for i in range(n+1)]
+    answer = 0
+    connect = [[] for _ in range(n+1)]
+    visited = [False for _ in range(n+1)]
+    
+    def bfs(node):
+        q = deque()
+        q.append(node)
+        while q:
+            N = q.popleft()
+            if visited[N]==False and connect[N]!=[]:
+                visited[N]=True
+                for c in connect[N]:
+                    q.append(c)
+                    
     for i in range(n):
         for j in range(n):
-            if computers[i][j]==1 and visited[j+1]==False:
-                visited[j+1]=True
-                visited[i+1]=True
-                bfs(j,visited)
-                answer+=1
-            else:
-                continue
+            if i!=j and computers[i][j]==1:
+                connect[i+1].append(j+1)
+
+    for a in range(1,n+1):
+        if visited[a]==False:
+            bfs(a)
+            answer+=1
     return answer
-    
